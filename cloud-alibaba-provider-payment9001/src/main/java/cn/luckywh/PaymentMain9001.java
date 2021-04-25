c4bc6b77f35bae4b9c1d0cc19b96a3bc57aec406
[luckywh] <wanghang8316@163.com>
2021-04-25 17:46:41 +0800

    springCloud的学习代码

---
+++ cloud-alibaba-provider-payment9001/src/main/java/cn/luckywh/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package cn.luckywh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动类
+ *
+ * @author W.
+ * @date 2021/4/25 12:11
+ */
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloud-alibaba-provider-payment9001/src/main/java/cn/luckywh/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package cn.luckywh.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试nacos
+ *
+ * @author W.
+ * @date 2021/4/25 14:30
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @GetMapping("/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return "nacos registry, serverPort: " + serverPort + "\t id: " + id;
+    }
+
+}
