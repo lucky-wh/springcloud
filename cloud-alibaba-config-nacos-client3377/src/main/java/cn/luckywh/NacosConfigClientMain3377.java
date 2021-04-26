7fb27e4ddbb33a50e8683a0a9909fd59e572d12f
[luckywh] <wanghang8316@163.com>
2021-04-26 08:21:23 +0800

    springCloud的学习代码

---
+++ cloud-alibaba-config-nacos-client3377/src/main/java/cn/luckywh/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package cn.luckywh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * nacos配置中心config
+ *
+ * @author W.
+ * @date 2021/4/25 20:00
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
