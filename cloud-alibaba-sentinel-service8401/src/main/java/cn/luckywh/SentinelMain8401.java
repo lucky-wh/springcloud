08049f797a82d61b46a201fb8f9337cc19a7a388
[luckywh] <wanghang8316@163.com>
2021-04-26 13:54:08 +0800

    springCloud的学习代码

---
+++ cloud-alibaba-sentinel-service8401/src/main/java/cn/luckywh/SentinelMain8401.java
@@ -0,0 +1,20 @@
+package cn.luckywh;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * sentinel的主启动类
+ *
+ * @author W.
+ * @date 2021/4/27 8:17
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SentinelMain8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelMain8401.class, args);
+    }
+}
---
+++ cloud-alibaba-sentinel-service8401/src/main/java/cn/luckywh/controller/FlowLimitController.java
@@ -0,0 +1,27 @@
+package cn.luckywh.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试sentinel
+ *
+ * @author W.
+ * @date 2021/4/27 8:18
+ */
+@RestController
+
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "----testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "----testB";
+    }
+
+}
